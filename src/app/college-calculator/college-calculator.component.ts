36a0a7f2cc81c498d1e2cc5366f4d701dbb6fdd1
Adam Davenport <adamdport@gmail.com>
2017-05-06 13:00:13 -0400

    copy

--- src/app/college-calculator/college-calculator.component.ts
+++ src/app/college-calculator/college-calculator.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-college-calculator',
   templateUrl: './college-calculator.component.html',
-  styleUrls: ['./college-calculator.component.css']
+  styleUrls: ['./college-calculator.component.scss']
 })
 export class CollegeCalculatorComponent implements OnInit {
 
